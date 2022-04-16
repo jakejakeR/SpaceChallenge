4f0fadb7f7a4ad66fbfe8fbd24ef9804f065f92d
Jakub Rogowski <jakerogowski33@gmail.com>
2022-04-16 18:05:01 +0200

    totalBudget class added. Simulation carried out. Final commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,32 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         Simulation simulation = new Simulation();
-        //System.out.println(simulation.loadU1(simulation.loadItems("phase-1.txt")).size());
-        System.out.println(simulation.loadU2(simulation.loadItems("phase-1.txt")).size());
+        // U1 simulation
+        // Loading items for Phase 1 and Phase 2
+        ArrayList<Item> itemsPhase1 = new ArrayList<>(simulation.loadItems("phase-1.txt"));
+        ArrayList<Item> itemsPhase2 = new ArrayList<>(simulation.loadItems("phase-2.txt"));
+        //Loading a fleet of U1 rockets for Phase 1 and then for Phase 2
+        ArrayList<Rocket> u1RocketsP1 = new ArrayList<>(simulation.loadU1(itemsPhase1));
+        ArrayList<Rocket> u1RocketsP2 = new ArrayList<>(simulation.loadU1(itemsPhase2));
+        System.out.println("U1 Rockets (fully loaded!) for Phase 1: " + u1RocketsP1.size());
+        System.out.println("U1 Rockets (fully loaded!) for Phase 2: " + u1RocketsP2.size());
+        // Running simulation and displaying total budget required
+        int totalBudgetForU1 = simulation.totalBudget(u1RocketsP1) + simulation.totalBudget(u1RocketsP2);
+        System.out.println("Total cost of U1 rockets for Phase 1 and 2: $" + totalBudgetForU1 + " million.");
+
+        // U2 simulation
+        //Loading a fleet of U1 rockets for Phase 1 and then for Phase 2
+        ArrayList<Rocket> u2RocketsP1 = new ArrayList<>(simulation.loadU2(itemsPhase1));
+        ArrayList<Rocket> u2RocketsP2 = new ArrayList<>(simulation.loadU2(itemsPhase2));
+        System.out.println("U2 Rockets (fully loaded!) for Phase 1: " + u2RocketsP1.size());
+        System.out.println("U2 Rockets (fully loaded!) for Phase 2: " + u2RocketsP2.size());
+        // Running simulation and displaying total budget required
+        int totalBudgetForU2 = simulation.totalBudget(u2RocketsP1) + simulation.totalBudget(u2RocketsP2);
+        System.out.println("Total cost of U2 rockets for Phase 1 and 2: $" + totalBudgetForU2 + " million.");
+
+        if(totalBudgetForU1 > totalBudgetForU2) System.out.println("Using U2 rockets is cheaper.");
+        else System.out.println("Using U1 rockets is cheaper.");
     }
 }
--- src/Simulation.java
+++ src/Simulation.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 
 public class Simulation {
     // Methods
-
     /**
      * This method loads all items from a text file and returns an ArrayList of Items.
      * Each line in the text file consists of the item name followed by = then its weigh in kg.
@@ -28,6 +27,14 @@ public class Simulation {
         return listOfItems;
     }
 
+    /**
+     * Takes the ArrayList of Items returned from loadItems and starts creating U1 rockets.
+     * It first tries to fill up 1 rocket with as many items as possible before creating a new rocket object
+     * and filling that one until all items are loaded.
+     * Then adds it to an ArrayList of Items
+     * @param listOfItems returned from loadItems
+     * @return the ArrayList of those U1 rockets that are fully loaded.
+     */
     public ArrayList<Rocket> loadU1(ArrayList<Item> listOfItems) {
         ArrayList<Rocket> rocketArrayList = new ArrayList<>();
         ArrayList<Item> itemsToBeTaken = new ArrayList<>(listOfItems);
@@ -37,11 +44,14 @@ public class Simulation {
             for(Item item : listOfItems) {
                 if(rocketU1.canCarry(item)) {
                     rocketU1.carry(item);
-                    System.out.println(item.name + " added.");
+                    //System.out.println(item.name + " added.");
                     itemsToBeTaken.remove(item);
                 }
             }
-            rocketArrayList.add(rocketU1);
+            // Check if rocket is fully loaded. If yes, add rocket to rocketArrayList
+            if(rocketU1.currentWeight == rocketU1.maxWeight) {
+                rocketArrayList.add(rocketU1);
+            }
             rocketU1 = new U1();
             listOfItems = itemsToBeTaken;
             itemsToBeTaken = new ArrayList<>(listOfItems);
@@ -49,6 +59,14 @@ public class Simulation {
         return rocketArrayList;
     }
 
+    /**
+     * Takes the ArrayList of Items returned from loadItems and starts creating U2 rockets.
+     * It first tries to fill up 1 rocket with as many items as possible before creating a new rocket object
+     * and filling that one until all items are loaded.
+     * Then adds it to an ArrayList of Items
+     * @param listOfItems returned from loadItems
+     * @return the ArrayList of those U1 rockets that are fully loaded.
+     */
     public ArrayList<Rocket> loadU2(ArrayList<Item> listOfItems) {
         ArrayList<Rocket> rocketArrayList = new ArrayList<>();
         ArrayList<Item> itemsToBeTaken = new ArrayList<>();
@@ -58,14 +76,35 @@ public class Simulation {
             for(Item item : listOfItems) {
                 if(rocketU2.canCarry(item)) {
                     rocketU2.carry(item);
-                    System.out.println(item.name + " added.");
+                    //System.out.println(item.name + " added.");
                 } else itemsToBeTaken.add(item);
             }
-            rocketArrayList.add(rocketU2);
+            // Check if rocket is fully loaded. If yes, add rocket to rocketArrayList
+            if(rocketU2.currentWeight == rocketU2.maxWeight) {
+                rocketArrayList.add(rocketU2);
+            }
             rocketU2 = new U2();
             listOfItems = itemsToBeTaken;
             itemsToBeTaken = new ArrayList<>();
         }
         return rocketArrayList;
+    }
+
+    /**
+     * Takes an ArrayList of Rockets and calls launch and land methods for each of the rockets in the ArrayList.
+     * Every time a rocket explodes or crashes it will have to send that rocket again.
+     * All while keeping track of the total budget required to send each rocket safely to Mars.
+     * @param rocketArrayList that are loaded for each phase
+     * @return total budget in millions of $
+     */
+    public int totalBudget(ArrayList<Rocket> rocketArrayList) {
+        int totalBudget=0;
+
+        for(Rocket rocket : rocketArrayList) {
+            do {
+                totalBudget += rocket.cost;
+            } while(!(rocket.launch() && rocket.land()));
+        }
+        return totalBudget;
     }
 }
